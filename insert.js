7b68705114feebe91b4642015e552bc13b756f46
minling <mzhao725@gmail.com>
2015-10-23 02:01:18 -0400

    made public folder and organized files inside it, adds server.js file

--- insert.js
+++ insert.js
@@ -8,12 +8,3 @@ var connection = mysql.createConnection({
   database: 'articles'
 });
 connection.connect();
-
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error('error connecting: ' + err.stack);
-//     return;
-//   }
- 
-//   console.log('connected as id ' + connection.threadId);
-// });
---
+++ public/scripts/map.js
@@ -0,0 +1,8 @@
+
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.7127, lng: -74.0059},
+      zoom: 13
+    });
+  }
